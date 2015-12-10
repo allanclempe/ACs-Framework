61dd781a0d30fd3226808c1220c5c657222cb5c9
Allan Clempe <allan.clempe@gmail.com>
2015-12-10 19:26:08 -0200

    Jwt Middleware to redirect 500 error to 401 when token expiress

---
+++ src/ACs.Security.Jwt/JwtMiddleware500to401Error.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using System.IdentityModel.Tokens;
+using System.Threading.Tasks;
+
+namespace ACs.Security.Jwt
+{
+    public class JwtMiddleware500to401Error
+    {
+        RequestDelegate _next;
+
+        public JwtMiddleware500to401Error(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (SecurityTokenExpiredException)
+            {
+                context.Response.StatusCode = 401;
+            }
+
+        }
+    }
+}
+
