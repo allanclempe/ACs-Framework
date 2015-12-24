d66bd2ca435a23ec8526fa0016e22d128235dda1
Allan Clempe <allan.clempe@gmail.com>
2015-12-24 01:23:06 -0200

    Add infra smtpConfiguration and Migration1.cs

---
+++ examples/ACs.Framework.Web/Infra/SmtpConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ACs.Net.Mail;
+
+namespace ACs.Framework.Web.Infra
+{
+    public class SmtpConfiguration : ISmtpConfiguration
+    {
+        public bool Activated { get; set; }
+
+        public string From { get; set; }
+
+        public string Password { get; set; }
+
+        public int Port { get; set; }
+
+        public string Server { get; set; }
+
+        public int Timeout { get; set; }
+
+        public string UserName { get; set; }
+
+        public bool UseSSL { get; set; }
+    }
+}
---
+++ examples/ACs.Framework.Web/Migrations/Migration1.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACs.Framework.Web.Migrations
+{
+    [Migration(1)]
+    public class Migration1 : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Delete.Column("Url").FromTable("Foo");
+            Alter.Table("Foo").AddColumn("Email").AsString(255).NotNullable();
+            
+            Insert.IntoTable("Foo")
+                .Row(new { Name = "Something 1", Email = "user1@domain.com.br" });
+
+            Insert.IntoTable("Foo")
+                .Row(new { Name = "Something 2", Email = "user2@domain.com.br" });
+        }
+    }
+}
